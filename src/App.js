e5681dae78da4d6b2b95e89359e8d8f318a1df35
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-04-26 10:15:13 -0400

    Added the base login form, without any styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   constructor () {
     super();
@@ -32,10 +33,20 @@ class App extends Component {
         <button name="login" onClick={this.handleOpenModal}>Login</button>
         <Modal isOpen={this.state.showModal} contentLabel="Minimal Modal Example" className="Modal"
            overlayClassName="Overlay">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
+           <div className="Form">
+             <form>
+             <h1>Login</h1>
+              <label>
+                Email:
+                <input type="text" name="Email" />
+              </label><br></br>
+              <label>
+                Password:
+                <input type="text" name="Password" />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+            </div>
           <button onClick={this.handleCloseModal}>Close Modal</button>
         </Modal>
         <p className="App-intro">
