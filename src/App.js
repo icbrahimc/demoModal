a9f9eabe555d3c0ea40aeeb722969fbdba63e35c
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-04-28 23:35:14 -0400

    Passing data effectively

--- src/App.js
+++ src/App.js
@@ -3,16 +3,85 @@ import Modal from 'react-modal';
 import logo from './logo.svg';
 import './App.css';
 
+function LoginForm(props) {
+  return (
+    <form>
+    <h1>Login</h1>
+     <label>
+       Email:
+       <input type="text" name="Email" />
+     </label><br></br>
+     <label>
+       Password:
+       <input type="text" name="Password" />
+     </label><br></br>
+     <a href="#" name="Forgot Password">Forgot Password</a><br></br>
+     <input type="submit" value="Submit" /><br></br>
+   </form>
+  );
+}
+
+function ForgetPasswordForm(props) {
+  return (
+    <form>
+    <h1>Forget Password</h1>
+     <label>
+       Email:
+       <input type="text" name="Email" />
+     </label><br></br>
+     <label>
+       Password:
+       <input type="text" name="Password" />
+     </label><br></br>
+     <a href="#" name="Forgot Password">Forgot Password</a><br></br>
+     <input type="submit" value="Submit" /><br></br>
+   </form>
+  );
+}
+
+class JoinForm extends Component {
+  render() {
+    return (
+      <form>
+        <h1>Join</h1>
+         <label>
+           Email:
+           <input type="text" name="Email" />
+         </label><br></br>
+         <label>
+           Password:
+           <input type="text" name="Password" />
+         </label><br></br>
+         <a href="#" name="Forgot Password">Forgot Password</a><br></br>
+         <input type="submit" value="Submit" onSubmit={this.props.onChange}/><br></br>
+       </form>
+    )
+  }
+}
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
-      showModal: false
+      showModal: false,
+      currentForm: "joinForm"
     };
 
+    // this.handleJoinForm = this.handleJoinForm.bind(this);
+    // this.handleLoginForm = this.handleLoginForm.bind(this);
+    // this.handleForgetPasswordForm = this.han
+    this.handleLinks = this.handleLinks.bind(this);
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+//can you hear me
+// no sweat
+// what you trying to do
+
+// onClick={this.heandleLinks.bind()}
+  handleLinks () {
+    // var name = formName;
+    this.setState({ currentForm: "loginForm" });
   }
 
   handleOpenModal () {
@@ -24,6 +93,17 @@ class App extends Component {
   }
 
   render() {
+    // Render a new form every single time the user makes a change.
+    let displayedForm = null;
+    if (this.state.currentForm === "loginForm") {
+      displayedForm = <LoginForm />;
+    } else if (this.state.currentForm === "forgetForm") {
+      displayedForm = <ForgetPasswordForm />;
+    } else if (this.state.currentForm === "joinForm") {
+      displayedForm = <JoinForm onChange={this.handleCloseModal}/>;
+    } else {
+      displayedForm = <h1>Ibrahim</h1>
+    }
     return (
       <div className="App">
         <div className="App-header">
@@ -34,21 +114,8 @@ class App extends Component {
         <Modal isOpen={this.state.showModal} contentLabel="Minimal Modal Example" className="Modal"
            overlayClassName="Overlay">
            <div className="Form">
-             <form>
-             <h1>Login</h1>
-              <label>
-                Email:
-                <input type="text" name="Email" />
-              </label><br></br>
-              <label>
-                Password:
-                <input type="text" name="Password" />
-              </label><br></br>
-              <a href="#" name="Forgot Password">Forgot Password</a><br></br>
-              <input type="submit" value="Submit" /><br></br>
-            </form>
-            <a href="#" name="Social Media">Social Media Link</a><br></br>
-            
+            {displayedForm}
+            <a href="#" name="Social Media" onClick={this.handleLinks}>Social Media Link</a><br></br>
             </div>
           <button onClick={this.handleCloseModal}>Close Modal</button>
         </Modal>
